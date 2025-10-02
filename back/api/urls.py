0b20f6eac2f16a48bbe8f0c2d046c882b00e1d21
Gabriel Henrique Miranda Silva <37556731855@>
2025-10-02 11:17:42 -0300

    consertando erros

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,33 +1,26 @@
 from django.urls import path
-from .views import*
-# from .views import AutoresView, listar_autores
-#from .views import EditoraView, listar_editores
-#from .views import LivroVieW, listar_livros
+from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
+    path('autores/', AutoresView.as_view(), name='autores-list'), 
+    path('autor/<int:pk>', AutoresDetailView.as_view(), name='autores-detail'),
+    path('authors', listar_autores, name='Listar Autores'),
+   
+    path('editoras', EditorasView.as_view()),
+    path('editora/<int:pk>', EditorasDetailView.as_view()),
+   
+    path('livros', LivrosView.as_view()),
+    path('livro/<int:pk>', LivrosDetailView.as_view()),
     
-    # Autores
-     path('autores', AutoresView.as_view()),
-    # path('authors', listar_autores),
-    
-    ### GET e POST
-
-     # Editora
-     path('editores', EditoraView.as_view()),
-    # path('publisher', listar_editores),
-    
-     # Livros
-    path('livros', LivroVieW.as_view()),
-    #path('book', listar_livros),
-
-    
-    ### UPDATE e DELETE
-    path('autor/<int:pk>',AutoresDetailView.as_view()),
-    path('editora/<int:pk>', EditoraDetailView.as_view()),
-    path('livro/<int:pk>', LivroDetaiVieW.as_view()),
-
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('register/', RegisterView.as_view(), name='register'),
-]
+    
+] 
+
+
+
--- back/api/views.py
+++ back/api/views.py
@@ -15,115 +15,60 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 
 
-
-# Autores
-class AutoresView(ListCreateAPIView):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
-
 @api_view(['GET', 'POST'])
 def listar_autores(request):
-    if request.method  == 'GET':
+    if request.method=='GET':
         queryset = Autor.objects.all()
-        serializer = AutorSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = AutorSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializers = AutorSerializer(queryset, many=True)
+        return Response(serializers.data)
+    elif request.method=='POST':
+        serializers = AutorSerializer(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-        
-
-# Editores
-class EditoraView(ListCreateAPIView):
-    queryset = Editora.objects.all()
-    serializer_class = EditoraSerializer
-
-@api_view(['GET', 'POST'])
-def listar_editores(request):
-    if request.method == 'GET':
-        queryset = Editora.objects.all()
-        serializer = EditoraSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = EditoraSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-        
-
-# Livros
-class LivroVieW(ListCreateAPIView):
-    queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
-
-@api_view(['GET', 'POST'])
-def listar_livros(Request):
-    if Request.method == 'GET':
-        queryset = Livro.objects.all()
-        serializer = LivroSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif Request.method == 'POST':
-        serializer = LivroSerializer(data = Request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-        
-
-
-# %%%%%%%%%%%%%%%%%%%% Autores %%%%%%%%%%%%%%%%%%%%
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
-    # permission_classses =[IsAuthenticated]
-
+    # permission_classes =[IsAuthenticated]
+    
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['id', 'autor', 's_autor']      # Permite o filtro exato
     search_fields = ['autor', 's_autor']               # busca parcial: ?search=Jorge
     filterset_class = AutorFilter                     
-
+    
 class AutoresDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
-    permission_classes =[IsAuthenticated]
+    # permission_classes =[IsAuthenticated]
 
-
-# %%%%%%%%%%%%%%%%%%%% Editores %%%%%%%%%%%%%%%%%%%%
-
-class EditoraView(ListCreateAPIView):
+class EditorasView(ListCreateAPIView):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
-    #permission_classes =[IsAuthenticated]
-
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['id', 'nome']
-    search_fields = ['nome']  
-
-
-class EditoraDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = Editora.objects.all()
-    serializer_class = EditoraSerializer
-    permission_classes =[IsAuthenticated]
-
-# %%%%%%%%%%%%%%%%%%%% Livros %%%%%%%%%%%%%%%%%%%%
-
-class LivroVieW(ListCreateAPIView):
-    queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
-    permission_classes =[IsAuthenticated]
+    # permission_classes =[IsAuthenticated]
     
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['id'] #exato
+    search_fields = ['autor', 's_autor'] #não extato  
 
-class LivroDetaiVieW(RetrieveUpdateDestroyAPIView):
+
+class EditorasDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+    permission_classes =[IsAuthenticated]
+
+class LivrosView(ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    permission_classes =[IsAuthenticated]
+    # permission_classes =[IsAuthenticated]
+
+class LivrosDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    # permission_classes =[IsAuthenticated]
+
 
 
 class RegisterView(CreateAPIView):
@@ -140,6 +85,4 @@ class RegisterView(CreateAPIView):
             'tokens': {'refresh': str(refresh), 'access': str(refresh.access_token)}
         }, status=status.HTTP_201_CREATED)
         
-    
-
     
